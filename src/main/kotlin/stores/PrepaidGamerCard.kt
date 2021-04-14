5ad8eb4ca59e5639968ad33d98740eb95198e056
Saifur Rahman Mohsin <mohsin92@me.com>
2021-04-14 20:36:23 +0530

    fix: Weird SSL issue on PrepaidGamerCard site

--- src/main/kotlin/stores/PrepaidGamerCard.kt
+++ src/main/kotlin/stores/PrepaidGamerCard.kt
@@ -7,6 +7,7 @@ class PrepaidGamerCard: Store() {
 	private var url = "https://prepaidgamercard.com/product/playstation-5-console-ps5/"
 
 	override suspend fun check(): Boolean {
+		System.setProperty("https.protocols", "TLSv1,TLSv1.1,TLSv1.2")
 		val doc = Jsoup.connect(url).get()
 		val items = doc.select("#product-285346 > div > div.product-main > div > div.product-info.summary.col.col-fit.entry-summary.product-summary > form")
 		return items.size == 1
